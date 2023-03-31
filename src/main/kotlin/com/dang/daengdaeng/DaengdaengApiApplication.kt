aabadcf2cb7c18f57d05b7963dc6665037790b13
stella <yujin.lee@inbyu.com>
2023-03-31 13:13:54 +0900

    initial commit

---
+++ src/main/kotlin/com/dang/daengdaeng/DaengdaengApiApplication.kt
@@ -0,0 +1,11 @@
+package com.dang.daengdaeng
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaengdaengApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DaengdaengApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/dang/daengdaeng/DaengdaengApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dang.daengdaeng
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DaengdaengApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
